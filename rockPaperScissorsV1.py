cc06f1f169039d5c865780fe9263c5fc127fb0a2
EABene <benedikt.sandler@gmail.com>
2026-03-29 14:11:54 +0200

    add loop

--- rockPaperScissorsV1.py
+++ rockPaperScissorsV1.py
@@ -1,37 +1,40 @@
 import random
 
 valid_options = ["schere", "stein", "papier", "exit"]
+user_choice = ""
 
-while True:
-    user_choice = input("Wähle Schere, Stein oder Papier >> ")
-    if user_choice.lower() in valid_options:
-        break
-    else: print("Schreibfehler! Probiers nochmal.")
+while user_choice != "Exit":
+
+    while True:
+        user_choice = input("Wähle Schere, Stein oder Papier >> ")
+        if user_choice.lower() in valid_options:
+            break
+        else: print("Schreibfehler! Probiers nochmal.")
 
 
-user_choice = user_choice.capitalize()
-computer_choice = random.choice(["Schere", "Stein", "Papier"])
+    user_choice = user_choice.capitalize()
+    computer_choice = random.choice(["Schere", "Stein", "Papier"])
 
-if user_choice == "Exit":
-    print("Du hast das Programm verlassen.")
-    exit() 
+    if user_choice == "Exit":
+        print("Du hast das Programm verlassen.")
+        exit()
 
-print("Deine Auswahl: ", user_choice)
-print("Computer Auswahl: ", computer_choice)
+    print("Deine Auswahl: ", user_choice)
+    print("Computer Auswahl: ", computer_choice)
 
-#Scoring
+    #Scoring
 
-wins_against = {
-    "Schere": "Papier",
-    "Stein":  "Schere",
-    "Papier": "Stein",
-}
+    wins_against = {
+        "Schere": "Papier",
+        "Stein":  "Schere",
+        "Papier": "Stein",
+    }
 
-if user_choice == "Exit":
-    print()
-elif user_choice == computer_choice:
-    print("Unentschieden")
-elif wins_against[user_choice] == computer_choice:
-    print("Du hast gewonnen :)")
-else:
-    print("Du hast verloren :(")
+    if user_choice == "Exit":
+        print()
+    elif user_choice == computer_choice:
+        print("Unentschieden")
+    elif wins_against[user_choice] == computer_choice:
+        print("Du hast gewonnen :)")
+    else:
+        print("Du hast verloren :(")
