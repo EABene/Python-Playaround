6022b85ca5b35eb567afc19b4a54eaf784e6b01b
EABene <benedikt.sandler@gmail.com>
2026-04-01 17:54:02 +0200

    Fixed game 3

--- rockPaperScissorsV1.py
+++ rockPaperScissorsV1.py
@@ -1,46 +1,40 @@
 import random
 
+
 valid_options = ["schere", "stein", "papier", "exit"]
-user_choice = ""
-user_score, computer_score = 0, 0
-
-while user_choice != "Exit" or user_score < 3 or computer_score < 3:
-
-    while True:
-        user_choice = input("Wähle Schere, Stein oder Papier >> ")
-        if user_choice.lower() in valid_options:
-            break
-        else: print("Schreibfehler! Probiers nochmal.")
+user_choice: str = ""
 
 
-    user_choice = user_choice.capitalize()
-    computer_choice = random.choice(["Schere", "Stein", "Papier"])
+while True:
+    user_choice = input("Wähle Schere, Stein oder Papier >> ")
+    if user_choice.lower() in valid_options:
+        break
+    else: print("Schreibfehler! Probiers nochmal.")
 
-    if user_choice == "Exit":
-        print("Du hast das Programm verlassen.")
-        exit()
 
-    print("Deine Auswahl: ", user_choice)
-    print("Computer Auswahl: ", computer_choice)
+user_choice = user_choice.capitalize()
+computer_choice = random.choice(["Schere", "Stein", "Papier"])
 
-    #Scoring
+if user_choice == "Exit":
+    print("Du hast das Programm verlassen.")
+    exit()
 
-    wins_against = {
-        "Schere": "Papier",
-        "Stein":  "Schere",
-        "Papier": "Stein",
-    }
+print("Deine Auswahl: ", user_choice)
+print("Computer Auswahl: ", computer_choice)
 
-    if user_choice == "Exit":
-        print()
-    elif user_choice == computer_choice:
-        print("Unentschieden")
-        print("Score:")
-        print(f"Du: {user_score}; Com: {computer_score}")
-    elif wins_against[user_choice] == computer_choice:
-        print("Du hast gewonnen :)")
-        user_score += 1
-        print("Score:")
-        print(f"Du: {user_score}; Com: {computer_score}")
-    else:
-        print("Du hast verloren :(")
+#Scoring
+
+wins_against = {
+    "Schere": "Papier",
+    "Stein":  "Schere",
+    "Papier": "Stein",
+}
+
+if user_choice == "Exit":
+    print()
+elif user_choice == computer_choice:
+    print("Unentschieden")
+elif wins_against[user_choice] == computer_choice:
+    print("Du hast gewonnen :)")
+else:
+    print("Du hast verloren :(")
