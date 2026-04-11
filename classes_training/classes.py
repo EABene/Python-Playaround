95e27cdd6a2c818c80f55cb3391b749c265d9f16
EABene <benedikt.sandler@gmail.com>
2026-04-11 11:32:32 +0200

    set up classes training

---
+++ classes_training/classes.py
@@ -0,0 +1,19 @@
+# Einführung in Classes
+
+class Gitarre:
+    def __init__(self, marke, saitenanzahl):
+        self.marke = marke
+        self.saitenanzahl = saitenanzahl
+
+    def beschreibung(self):
+        return f"{self.marke} mit {self.saitenanzahl} Saiten"
+
+meine_gitarre = Gitarre("Fender", 6)
+print(meine_gitarre.beschreibung())  # → Fender mit 6 Saiten
+
+"""
+__init__ – der Konstruktor: wird automatisch beim Erstellen einer Instanz aufgerufen
+self – Referenz auf die aktuelle Instanz (entspricht this in anderen Sprachen)
+Attribut – eine Variable, die zu einer Instanz gehört (self.marke)
+Methode – eine Funktion innerhalb einer Klasse 
+"""
