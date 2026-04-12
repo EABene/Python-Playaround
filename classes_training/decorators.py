61fe37c6e87a737b784404ba5933825dc002a689
EABene <benedikt.sandler@gmail.com>
2026-04-12 21:10:18 +0200

    added Decorator Training file

---
+++ classes_training/decorators.py
@@ -0,0 +1,29 @@
+# write a program that orders ice cream and uses decorators to add stuff to the ice cream
+
+
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print("AND Sprinkles are added")
+    return wrapper
+
+def add_smarties(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print("AND Smarties are added")
+    return wrapper
+
+
+
+@add_smarties
+@add_sprinkles
+def get_ice_cream(flavour):
+    print(f"You get {flavour} Ice Cream!")
+
+
+# ----------------------------------------
+
+get_ice_cream("Stracciatella")
+print()
+get_ice_cream("Lemon")
+print()
