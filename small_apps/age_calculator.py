fd95207388bd0c2ff1c88601396de2780678a1b0
EABene <benedikt.sandler@gmail.com>
2026-04-16 08:54:11 +0200

    Preparation for some projects

---
+++ small_apps/age_calculator.py
@@ -0,0 +1,4 @@
+# Enter birth date
+# Output: "You are ## years and ## months old.
+# Whatever weeks, days
+# part of Roadmap.sh
---
+++ small_apps/unit_converter.py
@@ -0,0 +1,5 @@
+""" Length: millimeter, centimeter, meter, kilometer, inch, foot, yard, mile.
+
+Weight: milligram, gram, kilogram, ounce, pound.
+
+Temperature: Celsius, Fahrenheit, Kelvin."""
